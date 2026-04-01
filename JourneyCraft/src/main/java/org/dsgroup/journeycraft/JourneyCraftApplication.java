84a5203de84c22fab733250fbc36442b9cfe31ab
yian <2072405578@qq.com>
2026-04-01 17:48:44 +0800

    back-dev:architect:添加项目依赖，并初始化项目结构

---
+++ JourneyCraft/src/main/java/org/dsgroup/journeycraft/JourneyCraftApplication.java
@@ -0,0 +1,13 @@
+package org.dsgroup.journeycraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JourneyCraftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JourneyCraftApplication.class, args);
+    }
+
+}
---
+++ JourneyCraft/src/test/java/org/dsgroup/journeycraft/JourneyCraftApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dsgroup.journeycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JourneyCraftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
