ca973e6f4e510deeb307a90ff7b70333aff670ba
yian <2072405578@qq.com>
2026-04-07 16:46:05 +0800

    back-yian:doc:添加模块间调用规范和示例

---
+++ JourneyCraft/src/main/java/org/dsgroup/journeycraft/file/service/impl/FileServiceImpl.java
@@ -0,0 +1,21 @@
+package org.dsgroup.journeycraft.file.service.impl;
+
+import org.dsgroup.journeycraft.file.api.FileService;
+
+import java.io.File;
+import java.io.InputStream;
+
+/**
+ * 这里实现api包下的接口
+ */
+public class FileServiceImpl implements FileService {
+    /**
+    完成具体实现
+     */
+    @Override
+    public boolean uploadFile(InputStream fileData) {
+        // do upload file
+        // return result;
+        return true;
+    }
+}
